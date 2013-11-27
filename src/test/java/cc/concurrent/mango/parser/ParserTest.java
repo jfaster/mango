e757418157c2a096c3768c4977ac562d89184622
ash <liangyanghe@gmail.com>
2013-11-27 19:23:26 +0800

    语法分析

--- src/test/java/cc/concurrent/mango/parser/ParserTest.java
+++ src/test/java/cc/concurrent/mango/parser/ParserTest.java
@@ -9,7 +9,7 @@ public class ParserTest {
 
     @Test
     public void test() throws ParseException {
-        Parser p = new Parser("a=:1.abc.ed :2  sdf in ( :3.x.y.x  )");
+        Parser p = new Parser("a=:1.abc.ed :2 ab${:1}cd sdf in ( :3.x.y.x  )");
         p.parse().dump("");
     }
 
