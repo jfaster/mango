69ae117a2bda0dacbf2e57051dd1dd9ff960afa9
ash <liangyanghe@gmail.com>
2014-01-01 10:07:47 +0800

    in

--- src/main/java/cc/concurrent/mango/runtime/parser/ASTInParam.java
+++ src/main/java/cc/concurrent/mango/runtime/parser/ASTInParam.java
@@ -1,16 +1,19 @@
 package cc.concurrent.mango.runtime.parser;
 
+import cc.concurrent.mango.exception.EmptyParameterException;
+import cc.concurrent.mango.exception.NullParameterException;
+import cc.concurrent.mango.exception.structure.IncorrectParameterTypeException;
 import cc.concurrent.mango.runtime.RuntimeContext;
+import cc.concurrent.mango.util.IterableHolder;
 import com.google.common.base.CharMatcher;
 import com.google.common.collect.Lists;
 
-import java.lang.reflect.Array;
-import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
 /**
  * @author ash
@@ -42,21 +45,21 @@ public class ASTInParam extends SimpleNode {
 
     public List<Object> values(RuntimeContext context) {
         Object v = context.getPropertyValue(beanName, propertyName);
-        checkNotNull(v, "parameter " + getParamName() + " can't be null");
-        List<Object> values = Lists.newArrayList();
-        if (v.getClass().isArray()) { // 数组
-            int size = Array.getLength(v);
-            for (int i = 0; i < size; i++) {
-                values.add(Array.get(v, i));
-            }
-        } else if (v instanceof Collection) { // 集合
-            for (Object o : (Collection) v) {
-                values.add(o);
-            }
-        } else {
-            throw new IllegalArgumentException("parameter " + getParamName() + " must be array or instance of Collection");
+        if (v == null) {
+            throw new NullParameterException("parameter " + getParamName() + " can't be null");
         }
-        checkArgument(!values.isEmpty(), "value of parameter " + getParamName() + " can't be empty");
+        IterableHolder iterableHolder = new IterableHolder(v);
+        if (!iterableHolder.isIterable()) {
+            throw new IncorrectParameterTypeException("expected collection or array but " + v.getClass());
+        }
+        if (iterableHolder.isEmpty()) {
+            throw new EmptyParameterException("batchUpdate's parameter can't be empty");
+        }
+
+        List<Object> values = Lists.newArrayList();
+        for (Object obj : iterableHolder) {
+            values.add(obj);
+        }
         return values;
     }
 
