ca07c8c965223fdd9fd0a7531b8ccacc4f225f54
ash <liangyanghe@gmail.com>
2015-09-21 15:02:07 +0800

    @Results注解注释

--- src/main/java/org/jfaster/mango/annotation/Results.java
+++ src/main/java/org/jfaster/mango/annotation/Results.java
@@ -19,6 +19,48 @@ package org.jfaster.mango.annotation;
 import java.lang.annotation.*;
 
 /**
+ * 自定义数据库表字段到类属性的映射
+ *
+ * <p>比如我们需要将数据库user表中的user_id,user_name字段，分别映射到类User的id,
+ *
+ * <pre>
+ * interface UserDao {
+ *
+ *   {@literal@}Results({
+ *     {@literal@}Result(column = "user_id", property = "id"),
+ *     {@literal@}Result(column = "user_name", property = "name")
+ *   })
+ *   {@literal@}SQL("select user_id, user_name from user where user_id = :1")
+ *   public User getUserById(int id);
+ *
+ *   {@literal@}Results({
+ *     {@literal@}Result(column = "user_id", property = "id"),
+ *     {@literal@}Result(column = "user_name", property = "name")
+ *   })
+ *   {@literal@}SQL("select user_id, user_name from user where user_name = :1")
+ *   public User getUserByName(String name);
+ *
+ * }
+ * </pre>
+ *
+ * <P>我们还可以把该注解放在UserDao上，效果和上面的代码一样
+ *
+ * <pre>
+ * {@literal@}Results({
+ *   {@literal@}Result(column = "user_id", property = "id"),
+ *   {@literal@}Result(column = "user_name", property = "name")
+ * })
+ * interface UserDao {
+ *
+ *   {@literal@}SQL("select user_id, user_name from user where user_id = :1")
+ *   public User getUserById(int id);
+ *
+ *   {@literal@}SQL("select user_id, user_name from user where user_name = :1")
+ *   public User getUserByName(String name);
+ *
+ * }
+ * </pre>
+ *
  * @author ash
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
