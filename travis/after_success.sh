f9c3812f60e0b5d264eb18854f3690f5579b38ec
liangyanghe <liangyanghe@le.com>
2016-11-22 11:24:45 +0800

    支持1.6，1.7，1.8测试

---
+++ travis/after_success.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#    Copyright 2014 the original author or authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+# Get the Java version.
+# Java 1.6 will give 16.
+# Java 1.7 will give 17.
+# Java 1.8 will give 18.
+VER=`java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q'`
+echo "Java detected: ${VER}"
+
+if [ $VER == "18" ]; then
+  mvn clean test jacoco:report coveralls:report -Pjava18
+else
+  echo "Travis build skipped"
+fi
